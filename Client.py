995af582fd6334c58315863da4758e093932efc6
IBoneash <zhangqiv5@163.com>
2017-06-30 17:34:14 +0800

    add price

--- Client.py
+++ Client.py
@@ -1,65 +1,76 @@
 import xlrd
+import re
 
 file_name = 'test.xls'
 ob = xlrd.open_workbook(file_name)
 
 
-class FromRow(object):
-    def __init__(self, letter):
-        self.letter = letter
+class Input(object):
+    def __init__(self, xh='', dy='', gg='', ll='', tj='', gap=0):
+        self.xh = xh
+        self.dy = dy
+        self.gg = gg
+        self.ll = ll
+        self.tj = tj
+        self.gap = gap
+
+    def get_gap(self):
+        self.gap = self.tj % 0.5
+        return self.gap
+
+    def get_price(self):
+
+
+def get_int(num):
+    if num % 1 == 0:
+        num = str(int(num))
+    else:
+        num = str(num)
+    return num
 
 
 if __name__ == '__main__':
-    xh_input = raw_input('Input xh: ')
-    dy_input = raw_input('Input dy: ')
-    gg_input = raw_input('Input gg: ')
-
-    ll_input = raw_input('Input letters: ')
-
-    key_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                'v', 'w', 'x', 'y', 'z', 'aa', 'ab', 'ac', 'ad']
-    row_list = []
-    result = 0
     try:
-        for x in range(0, ob.nsheets):
-            sh = ob.sheet_by_index(x)
-            for i in range(0, sh.nrows):
-                if xh_input.upper() in sh.row_values(i) and gg_input.decode('utf-8') in sh.row_values(
-                        i) and (dy_input.decode('utf-8') in sh.row_values(i) or dy_input == ''):
-                    row_list = sh.row_values(i)
-                    key_list2 = key_list[0:len(row_list)]
-                    dic = dict(zip(key_list2, row_list))
-                    letter_list = list(ll_input)
-                    # for l in range(0, len(letter_list)):
-                    #     if letter_list[l] == 'a':
-                    #         letter_list[l] = letter_list[l] + letter_list[l + 1]
-                    #         del letter_list[l + 1]
-                    for a in letter_list:
-                        result += dic.get(a)
-                        print 'letter ' + a + ' is ' + str(dic.get(a))
-                    print 'Result = ', result
-                    break
-                elif i == sh.nrows - 1:
-                    print 'No such model in sheet ' + str(x + 1) + '!'
-                    break
+        while True:
+            data = Input()
+            data.xh = raw_input('Input xh: ').upper()
+            data.dy = raw_input('Input dy: ')
+            data.gg = raw_input('Input gg: ')
+            data.gg = data.gg.replace('*', u'\xd7')
+            data.tj = float(raw_input('Input price of copper: '))
+            data.tj = get_int(data.tj)
+            data.tj = u'\u94dc\u4ef7' + data.tj + u'\u4e07\u5143/\u5428'
+            data.ll = raw_input('Input letters: ')
+
+            key_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                        't', 'u',
+                        'v', 'w', 'x', 'y', 'z', 'aa', 'ab', 'ac', 'ad']
+            row_data = []
+            price_data = []
+            result = 0
+            for x in range(0, ob.nsheets):
+                sh = ob.sheet_by_index(x)
+                price_data = sh.row_values(1)
+                for i in range(0, sh.nrows):
+                    row_data = sh.row_values(i)
+                    if data.xh in row_data and data.gg in row_data and (
+                                    data.dy.decode('utf-8') in row_data or data.dy == ''):
+                        key_list2 = key_list[0:len(row_data)]
+                        dic = dict(zip(key_list2, row_data))
+                        price_dic = dict(zip(price_data, row_data))
+                        for price in price_dic.keys():
+                            if data.tj == price:
+                                print price
+                        letter_list = re.findall(r'a\w|\w', data.ll)
+                        for a in letter_list:
+                            result += dic.get(a)
+                            print 'letter ' + a + ' = ' + str(dic.get(a))
+                        length = int(raw_input('Enter length = '))
+                        print 'Result = %f X %d = %f' % (result, length, result * length)
+                        break
+                    elif i == sh.nrows - 1:
+                        print 'No such model in sheet ' + str(x + 1) + '!'
+                        break
+            raw_input('Press Enter to continue...\n')
     except Exception, e:
-        print 'no sheet', e
-
-
-
-
-
-
-
-
-
-        # nrows = sh.nrows    # ncols = sh.ncols  #  # print nrows, ncols  #
-# cell_value = sh.cell_value(1, 3)
-#
-# row_list = []
-#
-# # for i in range(1, 3):
-#     row_data = sh.row_values(i)
-#     row_list.append(row_data)
-#
-# print cell_value
+        print e
