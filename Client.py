a7c12a681ddfdaf8715a227eec12615aa146c463
IBoneash <zhangqiv5@163.com>
2017-08-14 12:45:49 +0800

    add multi dy input and temp save letters and length

--- Client.py
+++ Client.py
@@ -71,13 +71,16 @@ class Xls(object):
 
 # class input
 class Input(object):
-    def __init__(self, xh='', dy='', gg='', ll='', tj='', tjv=0):
+    def __init__(self, xh='', dy='', gg='', ll='', l='', tj='', tjv=0, tjl=None):
         self.xh = xh
         self.dy = dy
         self.gg = gg
         self.ll = ll
+        self.l = l
         self.tj = tj
         self.tjv = tjv
+        if tjl is None:
+            self.tjl = []
 
     # calculate the gap of copper price
     def get_gap(self):
@@ -116,79 +119,105 @@ if __name__ == '__main__':
             xl = Xls()
             dy_dic = dict(zip(range(1, len(xl.get_dy()) + 1), xl.get_dy()))
             print dy_dic
-            logger.info('dy_list = %s' % (dy_dic))
+            logger.info('dy_list = %s' % dy_dic)
 
             data = Input()
             data.xh = raw_input('Input xh: ').upper()
-            logger.info('Input xh: %s' % (data.xh))
+            logger.info('Input xh: %s' % data.xh)
 
             data.dy = raw_input('Input dy: ')
-            logger.info('Input dy: %s' % (data.dy))
+            logger.info('Input dy: %s' % data.dy)
             if data.dy != '':
                 data.dy = xl.get_dy()[int(data.dy) - 1]
             print data.dy
 
             data.gg = raw_input('Input gg: ')
-            logger.info('Input gg: %s' % (data.gg))
+            logger.info('Input gg: %s' % data.gg)
             data.gg = data.gg.replace('*', u'\xd7')
 
-            data.tj = float(raw_input('Input price of copper: '))
-            data.tjv = data.tj
-            data.tj = get_int(data.tj)
-            logger.info('Input price of copper: %f' % (data.tjv))
-            data.tj = u'\u94dc\u4ef7' + data.tj + u'\u4e07\u5143/\u5428'
+            tongjia = raw_input('Input price of copper(split by /): ')
+            tj = tongjia.split('/')
+            for p in tj:
+                data.tjl.append(float(p))
 
-            data.ll = raw_input('Input letters: ')
-            logger.info('Input letters: %s' % (data.ll))
+            length_tmp = ''
+            for data.tj in data.tjl:
+                # data.tj = float(raw_input('Input price of copper: '))
+                data.tjv = data.tj
+                data.tj = get_int(data.tj)
+                logger.info('Input price of copper: %f' % data.tjv)
+                data.tj = u'\u94dc\u4ef7' + data.tj + u'\u4e07\u5143/\u5428'
 
-            key_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                        't', 'u',
-                        'v', 'w', 'x', 'y', 'z', 'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag']
-            row_data = []
-            price_data = []
-            result = 0
+                if data.l == '':
+                    data.l = raw_input('Input letters: ')
+                    data.ll = data.l
+                else:
+                    data.l = raw_input('Input letters: %s ?' % data.ll)
+                    if data.l != '':
+                        data.ll = data.l
+                    else:
+                        data.l = data.ll
+                        pass
+                logger.info('Input letters: %s' % data.ll)
 
-            for x in range(0, xl.open().nsheets):
-                sh = xl.open().sheet_by_index(x)
-                price_data = xl.get_price_data(x)
-                for i in range(0, sh.nrows):
-                    row_data = sh.row_values(i)
-                    if data.xh in row_data and data.gg in row_data and (
-                                    data.dy.decode(type) in row_data or data.dy == ''):
-                        key_list2 = key_list[0:len(row_data)]
-                        dic = dict(zip(key_list2, row_data))
-                        price_dic = dict(zip(price_data, row_data))
+                key_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                            's',
+                            't', 'u',
+                            'v', 'w', 'x', 'y', 'z', 'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag']
+                row_data = []
+                price_data = []
+                result = 0
 
-                        # unit price = (1-percent) * x + percent * y
-                        per = (1 - ((data.get_gap() / 0.1) * 0.2)) * price_dic[data.get_price()] + ((
-                                                                                                        data.get_gap() / 0.1) * 0.2) * \
-                                                                                                   price_dic[
-                                                                                                       data.get_price_plus()]
-                        print 'Price per meter of %s is %f' % (data.tj, per)
-                        logger.info('Price per meter of %s is %f' % (data.tj, per))
+                for x in range(0, xl.open().nsheets):
+                    sh = xl.open().sheet_by_index(x)
+                    price_data = xl.get_price_data(x)
+                    for i in range(0, sh.nrows):
+                        row_data = sh.row_values(i)
+                        if data.xh in row_data and data.gg in row_data and (
+                                        data.dy.decode(type) in row_data or data.dy == ''):
+                            key_list2 = key_list[0:len(row_data)]
+                            dic = dict(zip(key_list2, row_data))
+                            price_dic = dict(zip(price_data, row_data))
 
-                        letter_list = re.findall(r'a\w|\w', data.ll)
-                        for a in letter_list:
-                            result += dic.get(a)
-                            print 'letter ' + a + ' = ' + str(dic.get(a))
-                            logger.info('letter ' + a + ' = ' + str(dic.get(a)))
-                        print 'Unit price = %f + %f = %f' % (per, result, per + result)
-                        logger.info('Unit price = %f + %f = %f' % (per, result, per + result))
+                            # unit price = (1-percent) * x + percent * y
+                            per = (1 - ((data.get_gap() / 0.1) * 0.2)) * price_dic[data.get_price()] + ((
+                                                                                                            data.get_gap() / 0.1) * 0.2) * \
+                                                                                                       price_dic[
+                                                                                                           data.get_price_plus()]
+                            print 'Price per meter of %s is %f' % (data.tj, per)
+                            logger.info('Price per meter of %s is %f' % (data.tj, per))
 
-                        length = int(raw_input('Enter length = '))
-                        logger.info('Enter length = %d' % (length))
+                            letter_list = re.findall(r'a\w|\w', data.ll)
+                            for a in letter_list:
+                                result += dic.get(a)
+                                print 'letter ' + a + ' = ' + str(dic.get(a))
+                                logger.info('letter ' + a + ' = ' + str(dic.get(a)))
+                            print 'Unit price = %f + %f = %f' % (per, result, per + result)
+                            logger.info('Unit price = %f + %f = %f' % (per, result, per + result))
 
-                        print 'Result = %f X %d = %f' % (per + result, length, (per + result) * length)
-                        logger.info('Result = %f X %d = %f' % (per + result, length, (per + result) * length))
-                        break
+                            if length_tmp == '':
+                                length_tmp = raw_input('Enter length = ')
+                                length = int(length_tmp)
+                            else:
+                                length_tmp = raw_input('Enter length = %s ?' % length)
+                                if length_tmp != '':
+                                    length = length_tmp
+                                else:
+                                    length_tmp = length
+                                    pass
+                            logger.info('Enter length = %d' % length)
 
-                if i == sh.nrows - 1:
-                    print 'No such model in sheet ' + str(x + 1) + '!'
-                    logger.info('No such model in sheet ' + str(x + 1) + '!')
-                    continue
+                            print 'Result = %f X %d = %f' % (per + result, length, (per + result) * length)
+                            logger.info('Result = %f X %d = %f' % (per + result, length, (per + result) * length))
+                            break
 
-            raw_input('Press Enter to continue...\n')
-            logger.info('Press Enter to continue...\n')
+                    if i == sh.nrows - 1:
+                        print 'No such model in sheet ' + str(x + 1) + '!'
+                        logger.info('No such model in sheet ' + str(x + 1) + '!')
+                        continue
+
+                raw_input('Press Enter to continue...\n')
+                logger.info('Press Enter to continue...\n')
 
     except Exception, e:
         print e
