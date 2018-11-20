005b4fa55307cc3f6f356cce51329159cab99c40
IBoneash <zhangqiv5@163.com>
2018-11-20 17:20:06 +0800

    add 3.5 to 6.0

--- Client.py
+++ Client.py
@@ -32,7 +32,7 @@ class Xls(object):
         except:
             self.isopenfailed = True
             self.xlrd_object = None
-            print ('Open %s faile \n' % self.xls_name)
+            print('Open %s faile \n' % self.xls_name)
             pass
         finally:
             pass
@@ -108,31 +108,31 @@ if __name__ == '__main__':
     try:
         while True:
             xl = Xls()
-            dy_dic = dict(zip(range(1, len(xl.get_dy())+1), xl.get_dy()))
-            print dy_dic
+            dy_dic = dict(zip(range(1, len(xl.get_dy()) + 1), xl.get_dy()))
+            print(dy_dic)
             logger.info('dy_list = %s' % (dy_dic))
 
             data = Input()
-            data.xh = raw_input('Input xh: ').upper()
+            data.xh = input('Input xh: ').upper()
             logger.info('Input xh: %s' % (data.xh))
 
-            data.dy = raw_input('Input dy: ')
+            data.dy = input('Input dy: ')
             logger.info('Input dy: %s' % (data.dy))
             if data.dy != '':
                 data.dy = xl.get_dy()[int(data.dy) - 1]
-            print data.dy
+            print(data.dy)
 
-            data.gg = raw_input('Input gg: ')
+            data.gg = input('Input gg: ')
             logger.info('Input gg: %s' % (data.gg))
             data.gg = data.gg.replace('*', u'\xd7')
 
-            data.tj = float(raw_input('Input price of copper: '))
+            data.tj = float(input('Input price of copper: '))
             data.tjv = data.tj
             data.tj = get_int(data.tj)
             logger.info('Input price of copper: %f' % (data.tjv))
             data.tj = u'\u94dc\u4ef7' + data.tj + u'\u4e07\u5143/\u5428'
 
-            data.ll = raw_input('Input letters: ')
+            data.ll = input('Input letters: ')
             logger.info('Input letters: %s' % (data.ll))
 
             key_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
@@ -140,6 +140,8 @@ if __name__ == '__main__':
                         'v', 'w', 'x', 'y', 'z', 'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag']
             row_data = []
             price_data = []
+            per_list = []
+            tj_list = []
             result = 0
 
             for x in range(0, xl.open().nsheets):
@@ -148,42 +150,58 @@ if __name__ == '__main__':
                 for i in range(0, sh.nrows):
                     row_data = sh.row_values(i)
                     if data.xh in row_data and data.gg in row_data and (
-                                    data.dy.decode('utf8') in row_data or data.dy == ''):
+                            data.dy in row_data or data.dy == ''):
                         key_list2 = key_list[0:len(row_data)]
                         dic = dict(zip(key_list2, row_data))
                         price_dic = dict(zip(price_data, row_data))
 
                         # unit price = (1-percent) * x + percent * y
-                        per = (1 - ((data.get_gap() / 0.1) * 0.2)) * price_dic[data.get_price()] + ((
-                                                                                                        data.get_gap() / 0.1) * 0.2) * \
-                                                                                                   price_dic[
-                                                                                                       data.get_price_plus()]
-                        print 'Price per meter of %s is %f' % (data.tj, per)
-                        logger.info('Price per meter of %s is %f' % (data.tj, per))
+                        if data.tjv == 0:
+                            for n in range(35, 61):
+                                data.tjv = n / 10.0
+                                per = (1 - ((data.get_gap() / 0.1) * 0.2)) * price_dic[data.get_price()] + ((
+                                                                                                                    data.get_gap() / 0.1) * 0.2) * \
+                                      price_dic[
+                                          data.get_price_plus()]
+                                per_list.append(per)
+                                tj_list.append(data.tjv)
+                                print('Price per meter of %s is %f' % (data.tjv, per))
+                                logger.info('Price per meter of %s is %f' % (data.tjv, per))
+
+                        else:
+                            per = (1 - ((data.get_gap() / 0.1) * 0.2)) * price_dic[data.get_price()] + ((
+                                                                                                                data.get_gap() / 0.1) * 0.2) * \
+                                  price_dic[
+                                      data.get_price_plus()]
+                            per_list.append(per)
+                            tj_list.append(data.tj)
+                            print('Price per meter of %s is %f' % (data.tj, per))
+                            logger.info('Price per meter of %s is %f' % (data.tj, per))
 
                         letter_list = re.findall(r'a\w|\w', data.ll)
                         for a in letter_list:
                             result += dic.get(a)
-                            print 'letter ' + a + ' = ' + str(dic.get(a))
+                            print('letter ' + a + ' = ' + str(dic.get(a)))
                             logger.info('letter ' + a + ' = ' + str(dic.get(a)))
-                        print 'Unit price = %f + %f = %f' % (per, result, per + result)
+                        print('Unit price = %f + %f = %f' % (per, result, per + result))
                         logger.info('Unit price = %f + %f = %f' % (per, result, per + result))
 
-                        length = int(raw_input('Enter length = '))
+                        length = int(input('Enter length = '))
                         logger.info('Enter length = %d' % (length))
 
-                        print 'Result = %f X %d = %f' % (per + result, length, (per + result) * length)
-                        logger.info('Result = %f X %d = %f' % (per + result, length, (per + result) * length))
+                        for p, z in zip(per_list, tj_list):
+                            print('Result = %f X %d = %f (%.1f)' % (p + result, length, (p + result) * length, z))
+                            logger.info('Result = %f X %d = %f (%.1f)' % (p + result, length, (p + result) * length, z))
                         break
 
                 if i == sh.nrows - 1:
-                    print 'No such model in sheet ' + str(x + 1) + '!'
+                    print('No such model in sheet ' + str(x + 1) + '!')
                     logger.info('No such model in sheet ' + str(x + 1) + '!')
                     continue
 
-            raw_input('Press Enter to continue...\n')
+            input('Press Enter to continue...\n')
             logger.info('Press Enter to continue...\n')
 
-    except Exception, e:
-        print e
+    except Exception as e:
+        print(e)
         logger.info(e)
