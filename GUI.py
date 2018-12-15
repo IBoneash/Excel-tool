864def8dffaf62cb4dbaf5b98b9b8ceb2bfba082
IBoneash <zhangqiv5@163.com>
2018-12-15 22:20:30 +0800

    update

--- GUI.py
+++ GUI.py
@@ -3,93 +3,250 @@ from new_client import *
 
 
 def set_file_name():
+    global xls
+    global voltage, row_dict, price, multi_selection
+    voltage = None
+    row_dict = None
+    price = None
+    multi_selection = None
+    voltage_list_box.delete(0, END)
+    model_list_box.delete(0, END)
+    price_list_box.delete(0, END)
+    spe_list_box.delete(0, END)
     excel_file_name = file_name.get()
     xls = Xls(excel_file_name)
     voltage_list = xls.get_voltage()
     for item in voltage_list:
         voltage_list_box.insert(END, item)
+    voltage_list_box.insert(0, '无')
 
     model_list = xls.get_voltage(var='型号')
     for item in model_list:
         model_list_box.insert(END, item)
 
-    spe_list = xls.get_voltage(var='规格')
-    for item in spe_list:
-        spe_list_box.insert(END, item)
+    price_list = xls.get_price()
+    for item in price_list:
+        price_list_box.insert(END, item)
+    return xls
+
+
+def set_multi_selections():
+    try:
+        global row_dict
+        multi_list_box.delete(0, END)
+        if voltage and voltage != '无':
+            multi_selections_list, row_dict = xls.get_row(model, specification, voltage)
+            if multi_selections_list:
+                for item in multi_selections_list:
+                    multi_list_box.insert(END, item)
+        elif voltage == '无':
+            multi_selections_list, row_dict = xls.get_row(model, specification)
+            if multi_selections_list:
+                for item in multi_selections_list:
+                    multi_list_box.insert(END, item)
+        else:
+            result_txt.insert(END, '{}\n'.format('请选择电压.'))
+    except TypeError:
+        result_txt.insert(END, '{}\n'.format('该组合无效!请使用Excel中正确的"电压","型号","规格"组合.'))
+    except NameError:
+        result_txt.insert(END, '{}\n'.format('该组合无效!请使用Excel中正确的"电压","型号","规格"组合.'))
+    except Exception as e:
+        result_txt.insert(END, '{}\n'.format(e))
 
 
 def get_voltage(event):
-    return voltage_list_box.get(voltage_list_box.curselection())
+    global voltage
+    voltage = voltage_list_box.get(voltage_list_box.curselection())
+    spe_list_box.select_clear(0, END)
+    # result_txt.insert(END, '{}\n'.format(voltage))
 
 
 def get_model(event):
-    return model_list_box.get(model_list_box.curselection())
+    global model
+    model = model_list_box.get(model_list_box.curselection())
+    spe_list = xls.get_spe(model)
+    spe_list_box.delete(0, END)
+    for item in spe_list:
+        spe_list_box.insert(END, item)
+    # result_txt.insert(END, '{}\n'.format(model))
 
 
 def get_specification(event):
-    return spe_list_box.get(spe_list_box.curselection())
+    global specification
+    specification = spe_list_box.get(spe_list_box.curselection())
+    set_multi_selections()
+
+
+def get_price(event):
+    global price
+    price = price_list_box.get(price_list_box.curselection())
+    # result_txt.insert(END, '{}\n'.format(price))
+
+
+def get_multi_selections(event):
+    global multi_selection
+    multi_selection_list = []
+    select_tuple = multi_list_box.curselection()
+    for i in select_tuple:
+        multi_selection_list.append(multi_list_box.get(i))
+    multi_selection = tuple(multi_selection_list)
+    # result_txt.insert(END, '{}\n'.format(multi_selections))
+
+
+def get_unit_price():
+    try:
+        if row_dict and multi_selection and price:
+            unit_price = xls.get_unit_price(row_dict, price, multi_selection)
+            result_txt.insert(END, '计算结果{}万元/吨\n'.format(unit_price))
+            result_txt.see(END)  # 一直显示最新的一行
+            result_txt.update()
+        elif row_dict and price and not multi_selection:
+            unit_price = xls.get_unit_price(row_dict, price)
+            result_txt.insert(END, '计算结果{}万元/吨\n'.format(unit_price))
+            result_txt.see(END)  # 一直显示最新的一行
+            result_txt.update()
+        elif not row_dict:
+            result_txt.insert(END, '{}\n'.format('该组合无效!请使用Excel中正确的"电压","型号","规格"组合.'))
+            result_txt.see(END)  # 一直显示最新的一行
+            result_txt.update()
+        elif not price:
+            result_txt.insert(END, '{}\n'.format('请从铜价列表中选择铜价.'))
+            result_txt.see(END)  # 一直显示最新的一行
+            result_txt.update()
+        else:
+            result_txt.insert(END, '{}\n'.format('该组合无效!请使用Excel中正确的"电压","型号","规格"组合.'))
+            result_txt.see(END)  # 一直显示最新的一行
+            result_txt.update()
+    except NameError:
+        result_txt.insert(END, '{}\n'.format('请先加载Excel文件.'))
+        result_txt.see(END)  # 一直显示最新的一行
+        result_txt.update()
+    except Exception as e:
+        result_txt.insert(END, '{}\n'.format(e))
+        result_txt.see(END)  # 一直显示最新的一行
+        result_txt.update()
+    # result_txt.insert(END, '{}\n'.format(unit_price))
+
+
+def clear_result():
+    result_txt.delete('1.0', 'end')
 
 
 root = Tk()
 root.title("Excel tool")
-root.geometry("700x500")
-root.resizable(width=True, height=True)
+root.geometry("1000x500")
+root.resizable(width=False, height=False)
 
 # 显示电压列表
 Label(root, text="电压列表", font=('Arial', 10)).grid(row=0, column=0)
 voltage_list_display = StringVar()
-voltage_list_box = Listbox(root, listvariable=voltage_list_display)
+voltage_list_box = Listbox(root, listvariable=voltage_list_display, exportselection=False)
 voltage_list_box.bind('<ButtonRelease-1>', get_voltage)
-voltage_list_box.grid(row=1, column=0)
+voltage_list_box.grid(row=1, column=0, sticky=E)
+
+# 显示电压滚动条
+scrl_voltage = Scrollbar(root)
+scrl_voltage.grid(row=1, column=1, sticky=W + N + S)
+voltage_list_box.configure(yscrollcommand=scrl_voltage.set)
+scrl_voltage['command'] = voltage_list_box.yview
 
 # 显示型号列表
-Label(root, text="型号列表", font=('Arial', 10)).grid(row=0, column=1)
+Label(root, text="型号列表", font=('Arial', 10)).grid(row=0, column=2)
 model_list_display = StringVar()
-model_list_box = Listbox(root, listvariable=model_list_display)
+model_list_box = Listbox(root, listvariable=model_list_display, exportselection=False)
 model_list_box.bind('<ButtonRelease-1>', get_model)
-model_list_box.grid(row=1, column=1)
+model_list_box.grid(row=1, column=2, sticky=E)
 
-# 显示滚动条
+# 显示型号滚动条
 scrl_model = Scrollbar(root)
-scrl_model.grid(row=1, column=2, sticky=N + S)
+scrl_model.grid(row=1, column=3, sticky=W + N + S)
 model_list_box.configure(yscrollcommand=scrl_model.set)
 scrl_model['command'] = model_list_box.yview
 
 # 显示规格列表
-Label(root, text="规格列表", font=('Arial', 10)).grid(row=0, column=3)
+Label(root, text="规格列表", font=('Arial', 10)).grid(row=0, column=4)
 spe_list_display = StringVar()
-spe_list_box = Listbox(root, listvariable=spe_list_display)
+spe_list_box = Listbox(root, listvariable=spe_list_display, exportselection=False)
 spe_list_box.bind('<ButtonRelease-1>', get_specification)
-spe_list_box.grid(row=1, column=3)
+spe_list_box.grid(row=1, column=4, sticky=E)
 
-# 显示滚动条
+# 显示规格滚动条
 scrl_spe = Scrollbar(root)
-scrl_spe.grid(row=1, column=4, sticky=N + S)
+scrl_spe.grid(row=1, column=5, sticky=W + N + S)
 spe_list_box.configure(yscrollcommand=scrl_spe.set)
 scrl_spe['command'] = spe_list_box.yview
 
 # 显示铜价列表
-Label(root, text="铜价列表", font=('Arial', 10)).grid(row=0, column=3)
-spe_list_display = StringVar()
-spe_list_box = Listbox(root, listvariable=spe_list_display)
-spe_list_box.bind('<ButtonRelease-1>', get_specification)
-spe_list_box.grid(row=1, column=3)
+Label(root, text="铜价列表", font=('Arial', 10)).grid(row=0, column=6)
+price_list_display = StringVar()
+price_list_box = Listbox(root, listvariable=price_list_display, exportselection=False)
+price_list_box.bind('<ButtonRelease-1>', get_price)
+price_list_box.grid(row=1, column=6, sticky=E)
 
-# 显示滚动条
-scrl_spe = Scrollbar(root)
-scrl_spe.grid(row=1, column=4, sticky=N + S)
-spe_list_box.configure(yscrollcommand=scrl_spe.set)
-scrl_spe['command'] = spe_list_box.yview
+# 显示铜价滚动条
+scrl_price = Scrollbar(root)
+scrl_price.grid(row=1, column=7, sticky=W + N + S)
+price_list_box.configure(yscrollcommand=scrl_price.set)
+scrl_price['command'] = price_list_box.yview
+
+# 显示附加列表
+Label(root, text="附加选项列表", font=('Arial', 10)).grid(row=2, column=0)
+multi_list_display = StringVar()
+multi_list_box = Listbox(root, listvariable=multi_list_display, selectmode=MULTIPLE)
+multi_list_box.bind('<ButtonRelease-1>', get_multi_selections)
+multi_list_box.grid(row=3, column=0, sticky=E)
+
+# 显示附加列表滚动条
+scrl_multi = Scrollbar(root)
+scrl_multi.grid(row=3, column=1, sticky=E + N + S)
+multi_list_box.configure(yscrollcommand=scrl_multi.set)
+scrl_multi['command'] = multi_list_box.yview
+
+# 显示附加列表横向滚动条
+scrl_multi = Scrollbar(root, orient=HORIZONTAL)
+scrl_multi.grid(row=4, column=0, sticky=E + W + N)
+multi_list_box.configure(xscrollcommand=scrl_multi.set)
+scrl_multi['command'] = multi_list_box.xview
+
+# 显示获取附加项按钮
+# Button(root, text="获取附加选项", command=set_multi_selections).grid(row=5, column=0)
 
 # 显示文件名输入框
 file_name = StringVar()
 file_name_entry = Entry(root, textvariable=file_name)
 file_name.set("test.xls")
 
-file_name_entry.grid(row=1, column=5)
+file_name_entry.grid(row=1, column=8, sticky=N)
 
 # 显示加载按钮
-Button(root, text="Load", command=set_file_name).grid(row=1, column=6)
+Button(root, text="Load", command=set_file_name).grid(row=1, column=9, sticky=N)
+
+# 显示结果显示框
+Label(root, text="计算结果", font=('Arial', 10)).grid(row=2, column=2, columnspan=7, sticky=W)
+result_txt = Text(root, height=15)
+result_txt.grid(row=3, column=2, columnspan=6, rowspan=2, sticky=W + E + N + S)
+
+# 显示结果滚动条
+scrl_result = Scrollbar(root)
+scrl_result.grid(row=3, column=7, rowspan=2, sticky=E + S + N)
+result_txt.configure(yscrollcommand=scrl_result.set)
+scrl_result['command'] = result_txt.yview
+
+# 显示获取单价
+Button(root, text="获取单价", command=get_unit_price).grid(row=3, column=8, sticky=W + N)
+
+# 显示清除结果按钮
+Button(root, text="清除结果", command=clear_result).grid(row=5, column=7, sticky=W + N)
+
+# 显示自定义铜价
+Label(root, text="自定义铜价", font=('Arial', 10)).grid(row=4, column=8, sticky=W)
+
+# 显示自定义铜价输入框
+customer_copper_price = StringVar()
+customer_copper_price_entry = Entry(root, textvariable=customer_copper_price)
+# customer_copper_price.set("")
+
+customer_copper_price_entry.grid(row=5, column=8, sticky=W)
 
 root.grid()
 root.mainloop()
--- new_client.py
+++ new_client.py
@@ -37,13 +37,15 @@ class Xls(object):
             table = self.open().sheet_by_index(sheet)
             nrows = table.nrows  # 行数
             ncols = table.ncols  # 列数
-            for col_no in range(1, ncols):
+            for col_no in range(0, ncols):
                 cols = table.col_values(col_no)
                 for col in cols:
                     if var in str(col).replace(' ', ''):
-                        for cell_no in range(2, len(cols)):
-                            if cols[cell_no] not in voltage_list:
+                        for cell_no in range(0, len(cols)):
+                            if cols[cell_no] and cols[cell_no] not in voltage_list and str(cols[cell_no]).replace(' ',
+                                                                                                                  '') != var:
                                 voltage_list.append(cols[cell_no])
+        log.debug("获取{}列表为:{}".format(var, voltage_list))
         return voltage_list
 
     def get_price(self, var='铜价'):
@@ -53,16 +55,101 @@ class Xls(object):
             table = self.open().sheet_by_index(sheet)
             nrows = table.nrows  # 行数
             ncols = table.ncols  # 列数
-            for row_no in range(1, nrows):
+            for row_no in range(0, nrows):
                 rows = table.row_values(row_no)
                 for row in rows:
                     if var in str(row).replace(' ', ''):
-                        for cell_no in range(1, len(rows)):
-                            if rows[cell_no] not in price_list:
+                        for cell_no in range(0, len(rows)):
+                            if var in rows[cell_no].replace(" ", "") and rows[cell_no] not in price_list:
                                 price_list.append(rows[cell_no])
+        log.debug("获取{}列表为:{}".format(var, price_list))
         return price_list
 
+    def get_row(self, model, specification, voltage=None):
+        row_key = []
+        row_value = []
+        sheet_no = self.open().nsheets
+        for sheet in range(0, sheet_no):
+            multi_selections = []
+            table = self.open().sheet_by_index(sheet)
+            nrows = table.nrows  # 行数
+            ncols = table.ncols  # 列数
+            for row_no in range(0, nrows):
+                row_0 = []
+                rows = table.row_values(row_no)
+                for item in rows:
+                    row_0.append(str(item).replace(" ", ""))
+                if '型号' in row_0 and '规格' in row_0:
+                    row_key = row_0
+                    for select in row_0:
+                        if select and '执行价格' not in select and '序号' not in select and '供应商' not in select and '型号' not in select and '电压' not in select and '规格' not in select and '铜价' not in select:
+                            multi_selections.append(select)
+                if voltage:
+                    if model in rows and voltage in rows and specification in rows:
+                        row_value = rows
+                        row_dict = dict(zip(row_key, row_value))
+                        log.debug(
+                            "获取model={},voltage={},specification={} ,整行数据为:{},复选项为{}".format(model, voltage,
+                                                                                             specification,
+                                                                                             rows, multi_selections))
+                        return multi_selections, row_dict
+                elif model in rows and specification in rows and '电压' not in row_key:
+                    row_value = rows
+                    row_dict = dict(zip(row_key, row_value))
+                    log.debug(
+                        "没有电压,获取model={},specification={} ,整行数据为:{},复选项为{}".format(model, specification, rows,
+                                                                                   multi_selections))
+                    return multi_selections, row_dict
+                else:
+                    log.debug('没有该组合数据!')
 
-if __name__ == '__main__':
-    xls = Xls()
-    print(xls.get_price(var='铜价'))
+    def get_unit_price(self, row_dict, copper_price, multi_selections=None):
+        unit_price = row_dict[copper_price]
+        log.info('{} 对应单价为 {}'.format(copper_price, unit_price))
+        if multi_selections:
+            for i in multi_selections:
+                log.info('{} 对应单价为 {}'.format(i, row_dict[i]))
+                unit_price += row_dict[i]
+            log.info('最后统计总单价为 {}'.format(unit_price))
+        return '{:.4f}'.format(unit_price)
+
+    def get_spe(self, model):
+        spe_key = []
+        spe_value = []
+        spe_list = []
+        sheet_no = self.open().nsheets
+        for sheet in range(0, sheet_no):
+            table = self.open().sheet_by_index(sheet)
+            nrows = table.nrows  # 行数
+            ncols = table.ncols  # 列数
+            for col_no in range(0, ncols):
+                cols = table.col_values(col_no)
+                for col in cols:
+                    if "型号" == str(col).replace(' ', ''):
+                        for cell_no in range(0, len(cols)):
+                            if cell_no:
+                                spe_key.append(cols[cell_no])
+                    if "规格" == str(col).replace(' ', ''):
+                        for cell_no in range(0, len(cols)):
+                            if cell_no:
+                                spe_value.append(cols[cell_no])
+        for i in range(0, len(spe_key)):
+            if spe_key[i] == model and spe_value not in spe_list:
+                spe_list.append(spe_value[i])
+        log.debug("获取型号,规格对应列表为:{}".format(spe_list))
+        return spe_list
+
+
+# if __name__ == '__main__':
+#     xls = Xls()
+    #     dic = {'序号': 2.0, '框架协议供应商': '保定京阳立津线缆制造有限公司', '型号': 'KYJV', '规格': '10×1.5', '铜价2.5万元/吨': 6.2216,
+    #            '铜价3万元/吨': 7.1628, '铜价3.5万元/吨': 8.0148, '铜价4万元/吨': 8.8271, '铜价4.5万元/吨': 9.6692, '铜价5万元/吨': 10.4519,
+    #            '铜价5.5万元/吨': 11.1057, '铜价6万元/吨': 11.829, '铜价6.5万元/吨': 12.5423, '铜价7.0万元/吨': 13.3051, '铜价7.5万元/吨': 14.0679,
+    #            '耐火加价\n(元/米）\n（FF型填0）': 1.7535, 'A级阻燃加价\n(元/米）': 0.4656, 'B级阻燃加价\n(元/米）': 0.3269, 'C级阻燃加价\n(元/米）': 0.1783,
+    #            '低烟无卤加价\n(元/米）（FF型填0）': 0.6638, '软线R加价\n(元/米)': 0.5647, '铠装22加价\n(元/米）': 1.0303, '铠装23加价\n(元/米）': 1.07,
+    #            '铠装32加价\n(元/米）': 2.447, '铠装33加价\n(元/米）': 4.0817, '防白蚁加价（元/米）': 0.4954,
+    #            '耐低温（－40℃）聚氯乙烯护套加价(元/米）（GG型、FF型填0）': 0.5152, '铜带屏蔽P2减价（元/米）（无屏蔽填0）': 0.0, '铝塑屏蔽P3减价（元/米）（无屏蔽填0）': 0.0}
+    #     # print(xls.get_voltage(var='型号'))
+    #     # print(xls.get_row('KYJV', '10×1.5'))
+    #     print(xls.get_unit_price(dict, '铜价4.5万元/吨', '耐低温（－40℃）聚氯乙烯护套加价(元/米）（GG型、FF型填0）'))
+    # xls.get_spe('KGG')
